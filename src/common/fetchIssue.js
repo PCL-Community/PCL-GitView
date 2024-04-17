b62194d8d8b8156814eff68e97b06b708e14f943
youzi-2333 <youzi5201086@163.com>
2024-04-17 16:38:27 +0800

    :poop: 测试代码

--- src/common/fetchIssue.js
+++ src/common/fetchIssue.js
@@ -2,9 +2,24 @@ function getQueryParam(name, url) {
     const urlParams = new URLSearchParams(url.split('?')[1]);
     return urlParams.get(name);
 }
+function fetchOne(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(async (response) => {
+                if (!response.ok) {
+                    reject({
+                        status: response.status,
+                        message: "Error on fetch data",
+                    });
+                }
+                const data = await response.json();
+                resolve(data);
+            })
+    })
+}
 export const fetchAllIssues = () => {
     return new Promise((resolve, reject) => {
-        const fetchIssues = (url, issues) => {
+        const fetchIssues = (url) => {
             fetch(url) // TODO：删了个 Header，别忘记加回去 
                 .then(async (response) => {
                     if (!response.ok) {
@@ -15,30 +30,23 @@ export const fetchAllIssues = () => {
                     }
                     // Prase Link Header, For Paging
                     const linkHeader = response.headers.get("link");
-                    let lastPageLink = null;
-                    if (linkHeader) {
-                        const links = linkHeader.split(",");
-                        console.debug(links);
-                        const linkLast = links.find((link) =>
-                            link.includes('rel="last"')
-                        );
-                        console.debug(linkLast)
-                        lastPageLink = linkLast
-                            .split(";")[0]
-                            .trim()
-                            .slice(1, -1);
-                        console.debug(lastPageLink)
-                        lastPage = getQueryParam("page", lastPageLink)
-                        console.debug(lastPage)
-                    }
+                    const linkLast = linkHeader.split(",").find((link) =>
+                        link.includes('rel="last"')
+                    );
+                    const lastPageLink = linkLast
+                        .split(";")[0]
+                        .trim()
+                        .slice(1, -1);
+                    const lastPage = getQueryParam("page", lastPageLink);
+                    console.debug(lastPage);
                     const data = await response.json();
-                    const combinedIssues = issues.concat(data);
+                    let combinedIssues = data;
                     if (lastPageLink) {
-                        for (let i = 1; i <= lastPage; i++) {
-                            fetchIssues(
-                                lastPageLink.replace(`page=${lastPage}`, `page=${i}`),
-                                combinedIssues
+                        for (let i = 1; i <= Number(lastPage); i++) {
+                            data = await fetchOne(
+                                lastPageLink.replace(`page=${lastPage}`, `page=${i}`)
                             );
+                            combinedIssues = combinedIssues.concat(data);
                         }
                     } else {
                         resolve(combinedIssues);
